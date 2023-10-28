f138199e1b4fea115a4d088b0067713bdd8bead5
nomideusz <b.dymet@gmail.com>
2023-10-28 19:14:44 +0200

    Env var test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
   import '../app.css';
-  import { Svane } from '@shipbit/svane';
-  import { browser } from '$app/environment';
+  // import { Svane } from '@shipbit/svane';
+  // import { browser } from '$app/environment';
 </script>
 
-{#if import.meta.env.DEV && browser}
+<!-- {#if import.meta.env.DEV && browser}
   <Svane />
-{/if}
+{/if} -->
 <slot />
