4abab0df75c5dadfdb51bf606e4d2c88126a1463
nomideusz <b.dymet@gmail.com>
2023-11-13 04:24:12 +0100

    Fix locale load issue.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
   import { browser } from '$app/environment';
   import '$lib/i18n';
   import { locale, waitLocale } from 'svelte-i18n';
-
+  locale.set('en');
   export async function load() {
     if (browser) {
       locale.set(window.navigator.language);
