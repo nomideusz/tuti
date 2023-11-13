fc55d77ab9e1b47c22ab11cd487f416d564ce374
nomideusz <b.dymet@gmail.com>
2023-11-13 04:03:11 +0100

    Fix locale load issue.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,12 +1,12 @@
 import { getCurrentUser } from '$lib/api';
 import { locale } from 'svelte-i18n';
 
-export async function handle({ event, resolve }) {
-  event.locals.user = await getCurrentUser(event.cookies.get('sessionid'));
+export function handle({ event, resolve }) {
+  event.locals.user = getCurrentUser(event.cookies.get('sessionid'));
   const lang = event.request.headers.get('accept-language')?.split(',')[0];
   if (lang) {
     locale.set(lang);
   }
-  const response = await resolve(event);
+  const response = resolve(event);
   return response;
 }
