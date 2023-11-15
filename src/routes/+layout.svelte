09d07fb93707f226b6d7a6ecc1e69bf269a290e0
nomideusz <b.dymet@gmail.com>
2023-11-15 20:41:24 +0100

    Add google analytics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,8 @@
   import { browser } from '$app/environment';
   import '$lib/i18n';
   import { locale, waitLocale } from 'svelte-i18n';
-  locale.set('pl');
+  import Analytics from '$lib/components/Analytics.svelte';
+  locale.set('en');
   export async function load() {
     if (browser) {
       locale.set(window.navigator.language);
@@ -15,6 +16,7 @@
   // import { browser } from '$app/environment';
 </script>
 
+<Analytics />
 <Modals>
   <div
     slot="backdrop"
